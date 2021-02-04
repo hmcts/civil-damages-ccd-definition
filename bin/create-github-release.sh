ab459aa0d53e24e7fa2e684e57e57c2f7170a444
Piotr Dudkiewicz <pd.dudkiewicz@gmail.com>
2021-02-04 16:26:43 +0000

    updated github release script with e2e tests

--- bin/create-github-release.sh
+++ bin/create-github-release.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 zip -r civil-damages-ccd-definition.zip ccd-definition
+zip -r civil-damages-e2e.zip e2e codecept.conf.js package.json steps.d.ts yarn.lock
 
 currentVersion=$(curl --silent "https://api.github.com/repos/hmcts/civil-damages-ccd-definition/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
 IFS='.' read -a versionParts <<< "$currentVersion"
@@ -11,6 +12,8 @@ gh release create ${nextVersion} \
   --title civil-damages-ccd-definition-v${nextVersion} \
   --notes https://build.platform.hmcts.net/job/HMCTS_Unspec/job/civil-damages-ccd-definition/job/master/3/ \
   civil-damages-ccd-definition.zip \
+  civil-damages-e2e.zip \
   build/ccd-release-config/civil-damages-ccd-definition-aat.xlsx
 
 rm civil-damages-ccd-definition.zip
+rm civil-damages-e2e.zip
