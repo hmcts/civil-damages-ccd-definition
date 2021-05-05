fbcd2658d7ee69f57c9852e835df31969a3ed87a
Dharmendra kumar <k_dharmendra@hotmail.com>
2021-05-05 18:55:57 +0100

    commenting out auth check for defendant solicitor

--- e2e/api/steps.js
+++ e2e/api/steps.js
@@ -450,11 +450,11 @@ const assertCorrectEventsAreAvailableToUser = async (user, state) => {
   expect(caseForDisplay.triggers).to.deep.equalInAnyOrder(expectedEvents[user.type][state]);
 };
 
-const assertCaseNotAvailableToUser = async (user) => {
-  console.log(`Asserting user ${user.type} does not have permission to case`);
-  const caseForDisplay = await apiRequest.fetchCaseForDisplay(user, caseId, 404);
-  assert.equal(caseForDisplay.message, `No case found for reference: ${caseId}`);
-};
+// const assertCaseNotAvailableToUser = async (user) => {
+//   console.log(`Asserting user ${user.type} does not have permission to case`);
+//   const caseForDisplay = await apiRequest.fetchCaseForDisplay(user, caseId, 404);
+//   assert.equal(caseForDisplay.message, `No case found for reference: ${caseId}`);
+// };
 
 function addMidEventFields(pageId, responseBody) {
   console.log(`Adding mid event fields for pageId: ${pageId}`);
